f3e4ed3944045def10361a07bc8561e1b71c0f06
vanessa <vanessacamahuali28@gmail.com>
2023-01-12 09:00:33 -0300

    arquivo calculadora e escopo

---
+++ app/src/calculadora.java
@@ -0,0 +1,11 @@
+public class Calculadora {
+    /**
+   * Este método é utilizado para somar dois números inteiros
+   * @param numeroUm este é o primeiro parâmetro do método
+   * @param numeroDois este é o segundo parâmetro do método
+   * @return int o resultado deste método é a soma dos dois números.
+   */
+    public int somar(int numeroUm, int numeroDois) {
+        return  numeroUm + numeroDois;
+    }
+}
